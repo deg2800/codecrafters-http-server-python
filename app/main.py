2ca957f214df791097181a49d9a2d5a92a446e19
Derrick Goodfriend <deg2800@mac.com>
2024-05-12 20:12:19 -0400

    4th stage

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
 import socket
 
+# Constants
 HOST = 'localhost'
 PORT = 4221
 
-def send_http_response(connection, status_code, reason_phrase):
+def send_http_response(connection, status_code, reason_phrase, body='', content_type='text/plain'):
     """Send an HTTP response to the client."""
-    response = f"HTTP/1.1 {status_code} {reason_phrase}\r\n\r\n"
+    body_bytes = body.encode('utf-8')
+    content_length = len(body_bytes)
+    headers = (
+        f"Content-Type: {content_type}\r\n"
+        f"Content-Length: {content_length}\r\n"
+    )
+    response = f"HTTP/1.1 {status_code} {reason_phrase}\r\n{headers}\r\n{body}"
     connection.sendall(response.encode('utf-8'))
 
 def main():
@@ -24,10 +31,13 @@ def main():
             print(request)
 
             request_line = request.split('\r\n')[0]
-            path = request_line.split(' ')[1]
+            method, path, _ = request_line.split()
 
             if path == '/':
                 send_http_response(connection, 200, "OK")
+            elif path.startswith('/echo/'):
+                echo_str = path[6:]
+                send_http_response(connection, 200, "OK", body=echo_str)
             else:
                 send_http_response(connection, 404, "Not Found")
 
